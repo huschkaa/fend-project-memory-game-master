e855d0e5e78a55bc07a2ce41907e3d0e9331fdf3
Andrew Huschka <huschkaa@gmail.com>
2017-11-16 19:32:54 -0600

    card game working properly and first iteration of timer added

--- js/app.js
+++ js/app.js
@@ -21,22 +21,26 @@ $('.deck').append(cardArray);
 
 //Function to show cards as they are clicked
 function cardClickHandler(event) {
-  $(this).attr('class','open card show');
-  let cardcount = $('ul').find('.open').length;
+  $(this).attr('class','open card show click');
+  let cardcount = $('ul').find('.click').length;
+
   console.log(cardcount);
 //Array to check class of cards that are open once two are picked and turn them a color
   if (cardcount == 2) {
-    let openClass = $('.open').find('i');
-    let classone = openClass[0];
-    let classtwo = openClass[1];
+    let openClass = $('.click').find('i');
+    let classone = openClass[0].className;
+    let classtwo = openClass[1].className;
     console.log(classone);
     console.log(classtwo);
 
     if (classone == classtwo){
-        $('.open').attr('class','open card show match');
+        $('.click').attr('class','open card show match');
     }
     else {
-        $('.open').attr('class','open card show miss');
+        $('.click').attr('class','open card show miss click');
+        setTimeout(function(){
+          $('.click').attr('class','card');
+        }, 999);
     }
   };
 };
@@ -65,3 +69,22 @@ $('.card').on('click', cardClickHandler);
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+ $('#my-button').on('click',function() {
+     $('#my-button').remove();
+     $('body').attr('class','success');
+ });
+
+
+//Timer to display time once first card is clicked
+$('.card').on('click',function() {
+var timerVar = setInterval(countTimer, 1000);
+var totalSeconds = 0;
+function countTimer() {
+   ++totalSeconds;
+   var minute = Math.floor(totalSeconds/60);
+   var seconds = totalSeconds - (minute*60);
+
+   document.getElementById("timer").innerHTML = minute + ":" + seconds;
+}
+});
