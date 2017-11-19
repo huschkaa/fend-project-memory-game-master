d497eb1e96cfbaad64148c2b4ba34d6212e7847e
Andrew Huschka <huschkaa@gmail.com>
2017-11-18 19:53:52 -0600

    Game functions properly and added first version of the modal

--- js/app.js
+++ js/app.js
@@ -14,6 +14,7 @@ function shuffle(array) {
 
 //Create array of 16 cards
 let cardArray = $('.card');
+let moves = 0; /* variable for tracking number of moves made */
 
 // Shuffle the 16 cards using function provided, create HTML, and add to the page
 shuffle(cardArray);
@@ -24,67 +25,83 @@ function cardClickHandler(event) {
   $(this).attr('class','open card show click');
   let cardcount = $('ul').find('.click').length;
 
-  console.log(cardcount);
 //Array to check class of cards that are open once two are picked and turn them a color
   if (cardcount == 2) {
     let openClass = $('.click').find('i');
     let classone = openClass[0].className;
     let classtwo = openClass[1].className;
-    console.log(classone);
-    console.log(classtwo);
 
+//increment the moves variable and update the count in html
+    moves++;
+    document.getElementById("moves").innerHTML = moves;
+
+//Remove one star after 11 moves, but before 16 moves
+    if (moves > 13 && moves < 16) {
+      $('.stars').find('#twostar').attr('class','star');
+      }
+//Remove two stars after 15 moves
+    if (moves >= 16 && moves < 20) {
+      $('.stars').find('#twostar').remove();
+      $('.stars').find('#onestar').remove();
+    }
+//remove three stars after 19 Moves
+    if (moves >= 20) {
+      $('.stars').find('#twostar').remove();
+      $('.stars').find('#onestar').remove();
+      $('.stars').find('#nostar').remove();
+    }
+
+//If function when the two cards are a match
     if (classone == classtwo){
         $('.click').attr('class','open card show match');
+        console.log($('.match').length)
+        return moves;
     }
+
+//Else function if the cards do not match
     else {
         $('.click').attr('class','open card show miss click');
-        setTimeout(function(){
-          $('.click').attr('class','card');
-        }, 999);
+          setTimeout(function(){ $('.click').attr('class','card'); }, 999);
+          return moves;
     }
   };
 };
+
 $('.card').on('click', cardClickHandler);
 
-
-//Add card to list of open cards when clicked
-
-
-//If two cards are clicked check to see if they match
-
-
-
-
-
-
-
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
-
- $('#my-button').on('click',function() {
-     $('#my-button').remove();
-     $('body').attr('class','success');
- });
-
-
 //Timer to display time once first card is clicked
-$('.card').on('click',function() {
-var timerVar = setInterval(countTimer, 1000);
-var totalSeconds = 0;
+let timerVar = setInterval(countTimer, 1000);
+let totalSeconds = 0;
 function countTimer() {
    ++totalSeconds;
-   var minute = Math.floor(totalSeconds/60);
-   var seconds = totalSeconds - (minute*60);
+   let minute = Math.floor(totalSeconds/60);
+   let seconds = totalSeconds - (minute*60);
 
    document.getElementById("timer").innerHTML = minute + ":" + seconds;
-}
+};
+
+//function when restart is clicked to shuffle board, restart clock, moves, and stars
+$('.restart').on('click', function () {
+  moves = 0;
+  totalSeconds = 0;
+  document.getElementById("moves").innerHTML = 0;
+  $('.stars').find('i').attr('class','fa fa-star');
+  $('.card').attr('class','card');
+  shuffle(cardArray);
+  $('.deck').append(cardArray);
+  return moves;
 });
+
+//Function to track moves, time, and stars once game is Won and pop up
+  document.getElementById("popup").innerHTML = "With " + moves + " moves and " + $('.fa-star').length + " stars, and ";
+  let mpopup = document.getElementById('mpopupBox');
+  let mpLink = document.getElementById("mpopupLink");
+  let close = document.getElementsByClassName("close")[0];
+
+  mpLink.onclick = function() {
+      mpopup.style.display = "block";
+  };
+
+  close.onclick = function() {
+      mpopup.style.display = "none";
+  };
